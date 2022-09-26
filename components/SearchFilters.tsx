9bec9f6c7d54a8c2d89b79a61bf6165d931cc86f
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2022-09-26 14:03:01 +0300

    work done

--- components/SearchFilters.tsx
+++ components/SearchFilters.tsx
@@ -4,44 +4,116 @@ import { MdCancel } from 'react-icons/md';
 import Image from "next/image";
 import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
 import { filterData, getFilterValues } from '../utils/filterData';
+import noresult from '../assets/images/noresult.svg';
+import { baseUrl, fetchApi } from "../utils/fetchApi";
 
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [locationData, setLocationData] = useState([]);
+  const [showLocations, setShowLocations] = useState(false);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const searchProperties = (filterValues: any) => {
-    console.log('filt', filterValues)
     const path = router.pathname;
-    const {query} = router;
+    const { query } = router;
 
     const values = getFilterValues(filterValues);
-    console.log('values', values);
 
     values.forEach((item) => {
-      query[item.name] = item.value
+      if (item.value && filterValues?.[item.name]) {
+        console.log('check', item.value, filterValues?.[item.name])
+        query[item.name] = item.value
+      }
     })
-     
-    console.log('values2 ', values);
 
-    router.push({pathname: path, query});
-    
+    router.push({ pathname: path, query });
+
   }
+
+  useEffect(() => {
+    if (searchTerm !== '') {
+      const fetchData = async () => {
+        setLoading(true);
+        const data = await fetchApi(`${baseUrl}/auto-complete?query=${searchTerm}`);
+        setLoading(false);
+        setLocationData(data?.hits);
+      };
+
+      fetchData();
+    }
+  }, [searchTerm]);
 
   return (
     <Flex flexWrap='wrap' bg="gray.100" p="4" justifyContent={'center'} >
-        {filters.map((filter: any) =>
-          <Box key={filter.queryName}>
-            <Select
-              placeholder={filter?.placeholder}
-              w="fit-content"
-              p="2"
-              onChange={(e) => searchProperties({ [filter?.queryName]: e.target.value })}   >
-              {filter?.items?.map((item: any) => (
-                <option value={item.value} key={item.value}>{item.name}</option>
-              ))}
-            </Select>
-          </Box>
+      {filters.map((filter: any) =>
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter?.placeholder}
+            w="fit-content"
+            p="2"
+            onChange={(e) => searchProperties({ [filter?.queryName]: e.target.value })}   >
+            {filter?.items?.map((item: any) => (
+              <option value={item.value} key={item.value}>{item.name}</option>
+            ))}
+          </Select>
+        </Box>
+      )}
+      <Flex flexDir='column'>
+        <Button onClick={() => setShowLocations(!showLocations)} border='1px' borderColor='gray.200' marginTop='2' >
+          Search Location
+        </Button>
+        {showLocations && (
+          <Flex flexDir='column' pos='relative' paddingTop='2'>
+            <Input
+              placeholder='Type Here'
+              value={searchTerm}
+              w='300px'
+              focusBorderColor='gray.300'
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            {searchTerm !== '' && (
+              <Icon
+                as={MdCancel}
+                pos='absolute'
+                cursor='pointer'
+                right='5'
+                top='5'
+                zIndex='100'
+                onClick={() => setSearchTerm('')}
+              />
+            )}
+            {loading && <Spinner margin='auto' marginTop='3' />}
+            {showLocations && (
+              <Box height='300px' overflow='auto'>
+                {locationData?.map((location: any) => (
+                  <Box
+                    key={location.id}
+                    onClick={() => {
+                      searchProperties({ locationExternalIDs: location.externalID });
+                      setShowLocations(false);
+                      setSearchTerm(location.name);
+                    }}
+                  >
+                    <Text cursor='pointer' bg='gray.200' p='2' borderBottom='1px' borderColor='gray.100' >
+                      {location.name}
+                    </Text>
+                  </Box>
+                ))}
+                {!loading && !locationData?.length && (
+                  <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5' >
+                    <Image src={noresult} />
+                    <Text fontSize='xl' marginTop='3'>
+                      Waiting to search!
+                    </Text>
+                  </Flex>
+                )}
+              </Box>
+            )}
+          </Flex>
         )}
+      </Flex>
     </Flex>
   )
 }
